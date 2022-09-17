86b12f1419316950a38bb71813eb25f0bc7de935
Nico <weiskopf.bionico@gmail.com>
2022-09-17 14:12:37 +0200

    change database from test to boardGames

--- controllers/mongoDB_operations.js
+++ controllers/mongoDB_operations.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const BoardGame = require('../model/BoardGames')
 
-const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`
+const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}?retryWrites=true&w=majority`
 
 mongoose.connect(mongodbConnection)
 const db = mongoose.connection
