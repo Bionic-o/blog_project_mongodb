d5f71682e8bfe772258ea62231a0463ea566cc5c
Nico <weiskopf.bionico@gmail.com>
2022-09-17 11:47:17 +0200

    implement GetMethod + helperFunction

--- controllers/mongoDB_operations.js
+++ controllers/mongoDB_operations.js
@@ -6,3 +6,23 @@ const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process
 mongoose.connect(mongodbConnection)
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection failed'))
+
+function _makeBoardGames(dbBoardGames) {
+    return {
+        id: dbBoardGames._id,
+        title: dbBoardGames.name,
+        author: dbBoardGames.author,
+        imgUrl: dbBoardGames.img_url,
+        richText: dbBoardGames.rich_text,
+        publisher: dbBoardGames.publisher
+    }
+}
+
+async function getBoardGames() {
+    const dbBoardGames = await BoardGame.find({})
+    return dbBoardGames.map((dbBoardGame) => _makeBoardGames(dbBoardGame))
+}
+
+module.exports = {
+    getBoardGames,
+}
