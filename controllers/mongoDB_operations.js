8aded487c447d01f71686fa65a2653e12fdd5c3a
a-lavi <ali.rahmani.63@icloud.com>
2022-09-17 12:12:58 +0200

    get date via mongoose

--- controllers/mongoDB_operations.js
+++ controllers/mongoDB_operations.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose')
 const BoardGame = require('../model/BoardGames')
 
 const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`
@@ -6,3 +6,20 @@ const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process
 mongoose.connect(mongodbConnection)
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection failed'))
+ */
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+async function main(){
+    const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`
+    const client = new MongoClient(uri);
+    try {
+    await client.connect();
+    } catch(e){
+        console.log(e)
+    } finally{
+        await client.close()
+    }
+}
+
+main().catch(console.err)
--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const app = express()
 app.use(express.json())
 app.set('view engine', 'ejs')
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+async function main(){
+    const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`
+    const client = new MongoClient(uri);
+    try {
+    await client.connect();
+
+    await listDatabases(client)
+    } catch(e){
+        console.log(e)
+    } finally{
+        await client.close()
+    }
+}
+
+main().catch(console.err)
+
+async function listDatabases(client){
+ const databasesList=   await client.db().admin().listDatabases()
+ console.log("Dtatabases: ")
+ databasesList.databases.forEach(db => {
+    console.log(`- ${db.name}`)
+ })
+
+
+}
 const port = process.env.PORT || 5050
 
 
