8b7d79454279114cc8d6f551054ffe3adfa2320d
a-lavi <ali.rahmani.63@icloud.com>
2022-10-12 20:55:14 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://georasam.github.io',
+    'https://master--glowing-centaur-a39f50.netlify.app'
 ];
 
 module.exports = allowedOrigins;
