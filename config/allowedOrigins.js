c415829619cc7b0018778900915d6e5f5b40be18
a-lavi <ali.rahmani.63@icloud.com>
2022-10-13 09:32:48 +0200

    update URL

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'https://georasam.github.io',
     'https://master--glowing-centaur-a39f50.netlify.app',
     'https://blog-project-frontend22.herokuapp.com',
-    'https://lucent-moonbeam-97f47c.netlify.app'
+    'https://lucent-moonbeam-97f47c.netlify.app',
+    'https://blog-test-nine.vercel.app'
 ];
 
 module.exports = allowedOrigins;
